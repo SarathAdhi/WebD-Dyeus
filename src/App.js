f9cb21e807edf25edf9fdcd57a0149f42942017f
SarathAdhi <s.sarathadhithya@gmail.com>
2022-02-19 12:04:20 +0530

    Source files -> App.js, index.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/body/Home';
+import Header from './components/header/Header';
+
+function App(){
+    return (
+        <>
+        <Header />
+        <Home />
+        </>
+    )
+}
+
+export default App;
